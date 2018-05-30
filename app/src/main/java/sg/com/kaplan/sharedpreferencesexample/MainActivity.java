c607e748771b8964fda9d00e94cee7bbbe93d0b9
William Ku <kucheech@gmail.com>
2018-05-30 20:24:24 +0800

    added switch

--- app/src/main/java/sg/com/kaplan/sharedpreferencesexample/MainActivity.java
+++ app/src/main/java/sg/com/kaplan/sharedpreferencesexample/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText name_input, score_input;
+    Switch s;
     SharedPreferences sharedPref;
 
     @Override
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPref = getPreferences(Context.MODE_PRIVATE);
 
+        s = (Switch) findViewById(R.id.switch1);
         name_input = (EditText) findViewById(R.id.input_name);
         score_input = (EditText) findViewById(R.id.input_score);
         Button button = (Button) findViewById(R.id.button);
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putString("name", name_input.getText().toString());
                 editor.putInt("high_score", Integer.parseInt(score_input.getText().toString()));
+                editor.putBoolean("music", s.isChecked());
                 editor.commit();
                 Toast.makeText(MainActivity.this, "Settings saved!", Toast.LENGTH_SHORT).show();
             }
@@ -43,5 +47,7 @@ public class MainActivity extends AppCompatActivity {
         name_input.setText(name);
         int score = sharedPref.getInt("high_score", 0);
         score_input.setText(Integer.toString(score));
+        Boolean music = sharedPref.getBoolean("music", false);
+        s.setChecked(music);
     }
 }

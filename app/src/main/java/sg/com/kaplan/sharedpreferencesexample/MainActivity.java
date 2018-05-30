04f15b89d4db30c1c0795226f5f14c5df5850929
William Ku <kucheech@gmail.com>
2018-05-30 19:41:39 +0800

    Initial commit

---
+++ app/src/main/java/sg/com/kaplan/sharedpreferencesexample/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.com.kaplan.sharedpreferencesexample;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name_input, score_input;
+    SharedPreferences sharedPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+
+        name_input = (EditText) findViewById(R.id.input_name);
+        score_input = (EditText) findViewById(R.id.input_score);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("name", name_input.getText().toString());
+                editor.putInt("high_score", Integer.parseInt(score_input.getText().toString()));
+                editor.commit();
+                Toast.makeText(MainActivity.this, "Settings saved!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name = sharedPref.getString("name", "kaplan");
+        name_input.setText(name);
+        int score = sharedPref.getInt("high_score", 0);
+        score_input.setText(Integer.toString(score));
+    }
+}
